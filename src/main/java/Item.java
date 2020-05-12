5d1dfc117665ba92e3624328f7db7f0b2139e708
heimgun <heidi.guneriussen@live.no>
2020-05-12 16:09:21 +0200

    Item methods created

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Item implements Package {
 
+    String item;
+
+    ArrayList<String> listOfItems = new ArrayList<String>();
+
+    public void fillList(){
+        listOfItems.add("Pizza");
+        listOfItems.add("Letter from your mom");
+        listOfItems.add("Brand new shoes");
+        listOfItems.add("Book");
+        listOfItems.add("A too small sweater");
+        listOfItems.add("Bills");
+    }
+
+    public int getRandomNumber(){
+        Random random = new Random();
+        int x = random.nextInt(listOfItems.size());
+        System.out.println(x);
+        return x;
+    }
 
     @Override
     public String getItem() {
-        return "Pizza";
+        fillList();
+        item = listOfItems.get(getRandomNumber());
+        System.out.println(item);
+        return item;
     }
 }
